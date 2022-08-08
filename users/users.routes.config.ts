ed5e805e9d897de48bea1e38afe44eb3c528c9a7
kimson147 <kimson1478@gmail.com>
2022-08-08 20:21:37 +0700

    initial commit

---
+++ users/users.routes.config.ts
@@ -0,0 +1,44 @@
+import { CommonRoutesConfig } from "../common/common.routes.config";
+import express from "express";
+
+export class UsersRoutes extends CommonRoutesConfig {
+  constructor(app: express.Application) {
+    super(app, "UsersRoutes");
+  }
+
+  configureRoutes() {
+    this.app
+      .route(`/users`)
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send("User List");
+      })
+      .post((req: express.Request, res: express.Response) => {
+        res.status(200).send("User Post");
+      });
+    this.app
+      .route(`/users/:user_id`)
+      .all(
+        (
+          req: express.Request,
+          res: express.Response,
+          next: express.NextFunction
+        ) => {
+          next();
+        }
+      )
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`GET requested for id ${req.params.userId}`);
+      })
+      .put((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PUT requested for id ${req.params.userId}`);
+      })
+      .patch((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PATCH requested for id ${req.params.userId}`);
+      })
+      .delete((req: express.Request, res: express.Response) => {
+        res.status(200).send(`DELETE requested for id ${req.params.userId}`);
+      });
+
+    return this.app;
+  }
+}
